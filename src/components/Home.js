fb680978b5c5e5c86794c37bf4fc895cbc9f8804
Marwah Bakoor <marwahbakoor@gmail.com>
2021-07-21 15:46:45 +0300

    Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
+import PollReview from './PollReview'
+
+import '../Style/Home.css'
 
 class Home extends Component {
     render(){
         return(
-            <div className='home'>
-                <section className="home-section">
+                <section className="section">
                     <div className="text">Home</div>
+                    <div className="home-pool-cards">
+                            <ul className="cards-titles">
+                                <li className="title selcted" >
+                                    <a className="selected"> Unanswered Questions </a> 
+                                </li>
+                                <li className="title" >
+                                    <a> Answered Questions </a>
+                                </li>
+                            </ul>
+                        <div className="poll-reviwe-dashboard">
+                            <PollReview />
+                        </div>
+
+                    </div>
+                    <div>
+
+                    </div>
                 </section>
-            </div>
         )
     }
 }
---
+++ src/components/PollReview.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+import '../Style/PollReview.css'
+
+class PollReview extends Component {
+    render(){
+        return(
+            <div className='poll-review'>
+                dslkdns
+            </div>
+        )
+    }
+}
+
+export default PollReview
