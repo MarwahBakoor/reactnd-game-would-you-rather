57f17ec1f2e75587b2d27061fb8a925d492f49d2
Marwah Bakoor <marwahbakoor@gmail.com>
2021-07-27 20:59:08 +0300

    try

--- src/components/PollResults.js
+++ src/components/PollResults.js
@@ -1,42 +1,41 @@
 import React,{Component} from 'react'
 import { connect } from 'react-redux'
 import '../Style/PollResults.css'
+import Avatar from '../images/Avatar1.png'
 import { formatQuestion } from '../utils/helpers'
 class PollResults extends Component {
-    
+
     render(){
-        const {avatarURL, name, optionOne, optionTwo} = this.props.question
-        const {answer, op1per, op2per} = this.props
         return(
             <div className='box'>
                 <div className='profile-img' >
-                     <img src={avatarURL} alt="profileImg"  />
+                     <img src={Avatar} alt="profileImg"  />
                 </div>
                 <div className="content">
                     <div className="title">
-                            Asked by {name}:
+                            Asked by Sarah Edo:
                     </div>
                     <div className="choices-container" >
 
                         <div className="choice winner">
-                            <h3> Would you rather be {optionOne} </h3>
+                            <h3> Would you rather be back-end deveolper </h3>
                             <div>
                                 <div className="progress">
-                                    <span className="progress-bar" style={{width: '50%' }} ></span>
+                                    <span className="progress-bar" style={{width:'50%'}} ></span>
                                 </div>
-                                <div className="percentage"> {op1per} </div>
+                                <div className="percentage"> 50% </div>
                             </div>
                             <div className="user-vote">
                                     <span>Your Vote</span>
                             </div>
                         </div>
                         <div className="choice">
-                            <h3> Would you rather be {optionTwo} </h3>
+                            <h3> Would you rather be front-end deveolper </h3>
                             <div>
                                 <div className="progress">
                                     <span className="progress-bar" style={{width:'50%'}} ></span>
                                 </div>
-                                <div className="percentage"> {op2per} </div> 
+                                <div className="percentage"> 50% </div> 
                             </div>
                             <div className="user-vote">
                                     <span>Your Vote</span>
@@ -54,16 +53,12 @@ function mapStateToProps({questions,authedUser,users},{id}){
     const question = questions[id]
     const author = users[question.author]
     const answer = question.optionOne.votes.includes(authedUser) ? 'optionOne' :'optionTwo'
-    const op1 = question.optionOne.votes.length
-    const op2 =  question.optionOne.votes.length
-    const op1per = op1/(op1+op2) * 100
-    const op2per = op2/(op1+op2) * 100
+    
     return {
         question: formatQuestion(question,author),
         authedUser,
-        answer,
-        op1per,
-        op2per
+        answer
+        
     }
 
 }
